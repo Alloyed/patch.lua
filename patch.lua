ff31cf0b9975a6f2b7dd9a702b7d5a5e351c45c7
Kyle McLamb <kjmclamb@gmail.com>
2016-06-17 19:53:30 -0400

    Refactor out dispatch

--- patch.lua
+++ patch.lua
@@ -11,7 +11,41 @@ local function shallow_copy(t)
 end
 
 local visit
-local function merge(orig, diff, mutate)
+
+local replace_mt  = {REPLACE=true}
+local remove_i_mt = {REMOVE_I=true}
+local insert_i_mt = {INSERT_I=true}
+local update_mt   = {UPDATE=true}
+
+local function update_type(orig, diff)
+	if getmetatable(diff) == replace_mt then
+		return 'explicit_replace'
+	elseif getmetatable(diff) == remove_i_mt then
+		return 'remove_i'
+	elseif getmetatable(diff) == insert_i_mt then
+		return 'insert_i'
+	elseif orig == patch.Nil then
+		return 'replace'
+	elseif getmetatable(diff) == update_mt then
+		return 'update'
+	elseif type(diff) == 'table' and type(orig) == 'table' then
+		return 'merge'
+	else
+		return 'replace'
+	end
+end
+
+local updaters = {}
+
+function updaters.replace(orig, diff)
+	return diff, orig
+end
+
+function updaters.explicit_replace(orig, diff)
+	return diff.v, orig
+end
+
+function updaters.merge(orig, diff, mutate)
 	if not next(diff) then -- empty diff
 		return orig, nil
 	end
@@ -31,16 +65,15 @@ local function merge(orig, diff, mutate)
 	return new, undo
 end
 
-local function remove_i(orig, i, mutate)
-	local new = orig
-	if mutate == false then
-		new = shallow_copy(orig)
-	end
-	local v = table.remove(new, i)
-	return new, patch.insert_i(i, v)
+function updaters.update(orig, diff)
+	return diff.fn(orig, unpack(diff.args, 1, diff.n)), patch.replace(orig)
 end
 
-local function insert_i(orig, i, v, mutate)
+function updaters.insert_i(orig, diff, mutate)
+	assert(orig ~= patch.Nil)
+	assert(type(orig) == 'table')
+
+	local i, v = diff.i, diff.v
 	local new = orig
 	if mutate == false then
 		new = shallow_copy(orig)
@@ -49,10 +82,19 @@ local function insert_i(orig, i, v, mutate)
 	return new, patch.remove_i(i)
 end
 
-local replace_mt  = {REPLACE=true}
-local remove_i_mt = {REMOVE_I=true}
-local insert_i_mt = {INSERT_I=true}
-local update_mt   = {UPDATE=true}
+function updaters.remove_i(orig, diff, mutate)
+	assert(orig ~= patch.Nil)
+	assert(type(orig) == 'table')
+
+	local i = diff.i
+
+	local new = orig
+	if mutate == false then
+		new = shallow_copy(orig)
+	end
+	local v = table.remove(new, i)
+	return new, patch.insert_i(i, v)
+end
 
 function visit(orig, diff, mutate)
 	if diff == patch.Nil then
@@ -67,25 +109,9 @@ function visit(orig, diff, mutate)
 		orig = patch.Nil
 	end
 
-	if getmetatable(diff) == replace_mt then
-		return diff.v, patch.replace(orig)
-	elseif getmetatable(diff) == remove_i_mt then
-		assert(orig ~= patch.Nil)
-		assert(type(orig) == 'table')
-		return remove_i(orig, diff.i, mutate)
-	elseif getmetatable(diff) == insert_i_mt then
-		assert(orig ~= patch.Nil)
-		assert(type(orig) == 'table')
-		return insert_i(orig, diff.i, diff.v, mutate)
-	elseif orig == patch.Nil then
-		return diff, orig
-	elseif getmetatable(diff) == update_mt then
-		return diff.fn(orig, unpack(diff.args, 1, diff.n)), patch.replace(orig)
-	elseif type(diff) == 'table' and type(orig) == 'table' then
-		return merge(orig, diff, mutate)
-	else
-		return diff, patch.replace(orig)
-	end
+	local t = update_type(orig, diff)
+
+	return updaters[t](orig, diff, mutate)
 end
 
 --- The "nil" updater. When you want to set a field to nil, use this instead of
@@ -97,16 +123,24 @@ patch.Nil = setmetatable({}, {NIL = true})
 --  updaters.
 --  @param v the new value
 function patch.replace(v)
+	if v == nil then return patch.Nil end
 	return setmetatable({v = v}, replace_mt)
 end
 
---- Returns a "remove_i" updater. This is equivalent to table.remove.
+--- Returns an "remove_i" updater. This is equivalent to calling
+--  `table.remove(tbl, i)` where `tbl` is the input field.
+--  @param i the index of the thing to remove
 function patch.remove_i(i)
 	assert(i == nil or type(i) == 'number')
 	return setmetatable({i = i}, remove_i_mt)
 end
 
---- Returns an "insert_i" updater. This is equivalent to table.insert()
+--- Returns an "insert_i" updater. This is equivalent to calling
+--  `table.insert(tbl, i, v)` where `tbl` is the input field. Note that the
+--  2-arg variant is not supported: either explicitly name the index or use a
+--  merge.
+--  @param i the index to insert `v` at
+--  @param v the value to insert
 function patch.insert_i(i, v)
 	assert(i == nil or type(i) == 'number')
 	return setmetatable({i = i, v = v}, insert_i_mt)
@@ -120,7 +154,9 @@ function patch.update(fn, ...)
 	return setmetatable({fn = fn, n = select('#', ...), args = {...}}, update_mt)
 end
 
--- joins multiple patches together
+--- NYI: Takes multiple patch objects and returns a single patch that has the
+--  same effect as applying all of them. Patches that change the same field in
+--  mutually exclusive ways are considered errors and return nil.
 function patch.join(...)
 	error("NYI")
 end
--- spec/patch_spec.lua
+++ spec/patch_spec.lua
@@ -1,88 +1,112 @@
 local patch = require 'patch'
 
 local function test(input)
-   local patched, undo = patch.apply(input.old, input.diff)
-   local unpatched = patch.apply(input.new, undo)
-   it("can be applied", function()
-      assert.same(input.new, patched)
-   end)
-   it("can be undone", function()
-      assert.same(input.old, unpatched)
-   end)
+	local util = require 'luassert.util'
+	local _old, _new = util.deepcopy(input.old), util.deepcopy(input.new)
+	local patched, undo = patch.apply(input.old, input.diff)
+	local unpatched = patch.apply(input.new, undo)
+	it("can be applied", function()
+		assert.not_equal(_new, input.new)
+		assert.same(_new, input.new)
+		assert.not_equal(input.new, patched)
+		assert.same(input.new, patched)
+	end)
+	it("can be undone", function()
+		assert.not_equal(_old, input.old)
+		assert.same(_old, input.old)
+		assert.not_equal(input.old, unpatched)
+		assert.same(input.old, unpatched)
+	end)
+
+	local T, T_undo
+	it("can be applied inplace", function()
+		T, T_undo = patch.apply_inplace(unpatched, input.diff)
+		assert.not_equal(_new, input.new)
+		assert.same(_new, input.new)
+		assert.equal(unpatched, T)
+		assert.same(input.new, T)
+	end)
+	it("can be undone inplace", function()
+		T, T_undo = patch.apply_inplace(T, T_undo)
+		assert.not_equal(_old, input.old)
+		assert.same(_old, input.old)
+		assert.equal(unpatched, T)
+		assert.same(input.old, T)
+	end)
 end
 
 describe("flat tables", function()
-   test {
-      old  = {foo = "bar"},
-      diff = {foo = "baz"},
-      new  = {foo = "baz"}
-   }
-   test {
-      old  = {2, 4, 5},
-      diff = {[2] = 'meme'},
-      new  = {2, "meme", 5}
-   }
+	test {
+		old  = {foo = "bar"},
+		diff = {foo = "baz"},
+		new  = {foo = "baz"}
+	}
+	test {
+		old  = {2, 4, 5},
+		diff = {[2] = 'meme'},
+		new  = {2, "meme", 5}
+	}
 end)
 
 describe("new keys", function()
-   test {
-      old  = {oh   = "baby"},
-      diff = {yeah = "okay"},
-      new  = {oh = "baby", yeah = "okay"}
-   }
+	test {
+		old  = {oh   = "baby"},
+		diff = {yeah = "okay"},
+		new  = {oh = "baby", yeah = "okay"}
+	}
 end)
 
 describe("nested tables", function()
-   test {
-      old  = {foo = { foo = "b", bar = "a"}},
-      diff = {foo = { foo = "c" }},
-      new  = {foo = { foo = "c", bar = "a"}}
-   }
+	test {
+		old  = {foo = { foo = "b", bar = "a"}},
+		diff = {foo = { foo = "c" }},
+		new  = {foo = { foo = "c", bar = "a"}}
+	}
 end)
 
 describe("tables with patch.replace()", function()
-   test {
-      old  = {foo = { foo = "b", bar = "a"}},
-      diff = {foo = patch.replace { foo = "c" }},
-      new  = {foo = { foo = "c"}}
-   }
+	test {
+		old  = {foo = { foo = "b", bar = "a"}},
+		diff = {foo = patch.replace { foo = "c" }},
+		new  = {foo = { foo = "c"}}
+	}
 end)
 
 describe("tables with patch.Nil", function()
-   test {
-      old  = {foo = { foo = "b", bar = "a"}},
-      diff = {foo = patch.Nil, bar = "hi"},
-      new  = {bar = "hi"}
-   }
+	test {
+		old  = {foo = { foo = "b", bar = "a"}},
+		diff = {foo = patch.Nil, bar = "hi"},
+		new  = {bar = "hi"}
+	}
 end)
 
 describe("tables with patch.update()", function()
-   local function add(a, b)
-      return a + b
-   end
-   test {
-      old  = {foo = 6900},
-      diff = {foo = patch.update(add, 42)},
-      new  = {foo = 6942}
-   }
+	local function add(a, b)
+		return a + b
+	end
+	test {
+		old  = {foo = 6900},
+		diff = {foo = patch.update(add, 42)},
+		new  = {foo = 6942}
+	}
 end)
 
 -- FIXME: This is probably surprising behavior.
 describe("tables with shared references", function()
-   local t = {chthulu ='bar'}
-   test {
-      old  = {foo = t, bar = t},
-      diff = {foo = {foo = "c" }},
-      new  = {foo = {foo = "c", chthulu = 'bar'}, bar = t}
-   }
+	local t = {chthulu ='bar'}
+	test {
+		old  = {foo = t, bar = t},
+		diff = {foo = {foo = "c" }},
+		new  = {foo = {foo = "c", chthulu = 'bar'}, bar = t}
+	}
 end)
 
 describe("tables with cycles", function()
-   local t = {chthulu ='bar'}
-   t.woah = t
-   test {
-      old  = {foo = t, bar = t},
-      diff = {foo = {foo = "c" }},
-      new  = {foo = {foo = "c", chthulu = 'bar', woah = t}, bar = t}
-   }
+	local t = {chthulu ='bar'}
+	t.woah = t
+	test {
+		old  = {foo = t, bar = t},
+		diff = {foo = {foo = "c" }},
+		new  = {foo = {foo = "c", chthulu = 'bar', woah = t}, bar = t}
+	}
 end)
